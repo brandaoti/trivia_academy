6c50bcab632319fa367aa319b32bfd4cfcee5caa
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-04 00:10:16 -0300

    Customizado TextButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,16 +23,25 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 40.0),
+            SizedBox(height: 24.0),
             Text('Trivia Academy'),
 
             SizedBox(height: 88.0),
-
             TextButton(
-              child: Text('Começar'),
+              child: Text(
+                'Começar',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14.0,
+                ),
+              ),
               style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.red,
+                primary: Color(0xFFF7F7F7),
+                backgroundColor: Color(0xFFDA0175),
+                padding: EdgeInsets.fromLTRB(60, 11, 60, 11),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
               onPressed: () {},
             ),
