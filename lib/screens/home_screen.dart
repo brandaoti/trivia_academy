349172578b0d3e8ae9d57a6797d754cc7cd078ad
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-03 23:50:31 -0300

    Criando estrutura da HomeScreen, add logo, texto e um botão

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,33 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            // ! Add Imagem, Text e um TextButton
+            SizedBox(height: 167.0),
+
+            // TODO, retirar o container apos criar layout
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Container(
+                color: Colors.white,
+                child: Image(
+                  image: AssetImage('assets/logo.png'),
+                  // color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 40.0),
+            Text('Trivia Academy'),
+
+            SizedBox(height: 88.0),
+
+            TextButton(
+              child: Text('Começar'),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.red,
+              ),
+              onPressed: () {},
+            ),
           ],
         ),
       ),
