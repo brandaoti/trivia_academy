59cb4e0bdb50d453ec696b4e62d41325de4e3c70
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-05 21:58:00 -0300

    Criando classe app_routes, centralizando caminho de navegação

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+// ! configurando caminho de navegação entre telas
+class AppRoutes {
+  //
+  static const LOADING_SCREEN = '/';
+  static const HOME_SCREEN = '/home_screen';
+  static const QUESTION_SCREEN = '/question_screen';
+}
