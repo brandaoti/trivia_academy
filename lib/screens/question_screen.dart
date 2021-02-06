3b5bf2020bbd8efec606f406a1800f31a76a4e52
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-05 21:33:37 -0300

    Criando tela de perguntas

---
+++ lib/screens/question_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Tela de perguntas
+class QuestionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
