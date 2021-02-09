ad8c0ab095dde20e280f77fca739bdad41d4ffd7
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-08 21:56:18 -0300

    Iniciando componentização das respostas

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,8 +1,7 @@
 // ! configurando caminho de navegação entre telas
 class AppRoutes {
   //
-  static const LOADING_SCREEN = '/';
-  static const HOME_SCREEN = '/home_screen';
-  static const QUESTION_SCREEN =
-      '/'; // TODO: trocar o caminho para /question_screen
+  static const LOADING_SCREEN = '/'; // ! A raiz vai ser a tela splash
+  static const HOME_SCREEN = '/'; // Todo, trocar por /home_screen
+  static const QUESTION_SCREEN = '/question_screen';
 }
