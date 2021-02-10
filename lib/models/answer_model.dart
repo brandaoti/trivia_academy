fe3ec83516d873f406888925532d6e0309182e63
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-09 21:52:10 -0300

    Criando class answer_model.dart

---
+++ lib/models/answer_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class AnswerModel {
+  final String title;
+  final int value;
+  // final int groupValue;
+
+  AnswerModel({
+    @required this.title,
+    @required this.value,
+    // @required this.groupValue,
+  });
+}
