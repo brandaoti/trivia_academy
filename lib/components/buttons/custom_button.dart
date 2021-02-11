4dbce399d528032a70f54cf2e4def7c8759061cf
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-11 01:14:15 -0300

    Criado botão e customizando

---
+++ lib/components/buttons/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  //
+  final String name;
+  final Function onPressed;
+  final Color primary, onPrimary;
+  final EdgeInsetsGeometry padding;
+  final BorderRadiusGeometry borderRadius;
+
+  //
+  const CustomButton({
+    Key key,
+    this.name,
+    this.primary,
+    this.onPrimary,
+    this.padding,
+    this.borderRadius,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(name),
+      style: ElevatedButton.styleFrom(
+        primary: primary,
+        onPrimary: onPrimary,
+        padding: padding,
+        shape: RoundedRectangleBorder(borderRadius: borderRadius),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
