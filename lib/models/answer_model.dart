28b55c127c7158ad4bd62e5fa0f21fedc441167f
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-12 00:58:12 -0300

    Atualizando classe answer_model.dart

--- lib/models/answer_model.dart
+++ lib/models/answer_model.dart
@@ -5,9 +5,18 @@ class AnswerModel {
   final int value;
   final int groupValue;
 
+  //
   AnswerModel({
     @required this.title,
     @required this.value,
     @required this.groupValue,
   });
+
+  //
+  AnswerModel copyWith({String title, int value, int groupValue}) =>
+      AnswerModel(
+        title: title ?? this.title,
+        value: value ?? this.value,
+        groupValue: groupValue ?? this.groupValue,
+      );
 }
