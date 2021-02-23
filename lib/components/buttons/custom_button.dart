7bc6eec3975412bfeeacb3c3200d6cc2b7435edd
Daniel Brandao <dannnbrandao@gmail.com>
2021-02-23 14:45:41 -0300

    add button customizado na home_screen

--- lib/components/buttons/custom_button.dart
+++ lib/components/buttons/custom_button.dart
@@ -5,16 +5,16 @@ class CustomButton extends StatelessWidget {
   final String name;
   final Function onPressed;
   final Color primary, onPrimary;
-  // final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry padding;
   final BorderRadiusGeometry borderRadius;
 
   //
   const CustomButton({
     Key key,
     @required this.name,
-    this.primary,
-    this.onPrimary,
-    // this.padding,
+    this.primary = const Color(0xFFDA0175),
+    this.onPrimary = const Color(0xFFF7F7F7),
+    this.padding = const EdgeInsets.fromLTRB(60, 11, 60, 11),
     this.borderRadius,
     @required this.onPressed,
   }) : super(key: key);
@@ -26,7 +26,7 @@ class CustomButton extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         primary: primary,
         onPrimary: onPrimary,
-        // padding: padding,
+        padding: padding,
         shape: RoundedRectangleBorder(borderRadius: borderRadius),
       ),
       onPressed: onPressed,
