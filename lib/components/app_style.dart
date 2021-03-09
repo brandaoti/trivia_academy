275e89b41c9bb0c0199af81dd1d1d6dc30181fd2
Daniel Brandao <dannnbrandao@gmail.com>
2021-03-09 00:22:46 -0300

    customização de textos

---
+++ lib/components/app_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:trivia_academy/components/colors/app_colors.dart';
+
+const LargeTextSize = 32.0;
+const MediumTextSize = 20.0;
+const BodyTextSize = 16.0;
+
+const String FontNameDefault = 'Montserrat';
+
+const Headline6TextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w800,
+  fontSize: LargeTextSize,
+  color: AppColors.headlineColor,
+);
+
+const Body1TextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: BodyTextSize,
+  color: Colors.red,
+);
